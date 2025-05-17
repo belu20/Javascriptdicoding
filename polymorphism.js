1400050839f07e7e55ef92d9d5832b97247c4f44
belu20 <isanseptian20@gmail.com>
2025-05-17 21:09:51 +0700

    polymorphism

---
+++ polymorphism.js
@@ -0,0 +1,34 @@
+class SmartPhone {
+  constructor(color, brand, model) {
+    this.color = color;
+    this.brand = brand;
+    this.model = model;
+  }
+
+  charging() {
+    console.log(`Charging ${this.model}`);
+  }
+}
+
+class Android extends SmartPhone {
+  //Overriding constructor
+  constructor(color, brand, model, device) {
+    super(color, brand, model);
+    this.device = device;
+  }
+
+  charging() {
+    //call method charging dari supeClass (smartphone)
+    super.charging();
+    console.log(`charging ${this.model} with fast charging`);
+  }
+
+  splitscreen() {
+    console.log("android have a behavier splitscreen");
+  }
+}
+
+const android = new Android("black", "b", "redmi 9t", "Smart TV");
+
+android.splitscreen();
+android.charging();
